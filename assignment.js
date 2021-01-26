7483637cc89ca9ebb86bf651e8b81eda70002840
firasel <mdrasel601390@gmail.com>
2021-01-26 23:26:27 +0600

    not any change

--- assignment.js
+++ assignment.js
@@ -44,6 +44,11 @@ function hotelCost(day) {
 
 // function to find the biggest name
 function megaFriend(arr) {
+    for(var i=0;i<arr.length;i++){
+        if(typeof arr[i]== 'number'){
+            return 'error'
+        }
+    }
     if (arr.length == 0) {
         return 'friends array not available value';
     } else {
